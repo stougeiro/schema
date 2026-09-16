6559e84f778af202e888057d21439c1c7dcd7717
S.Tougeiro <sidneytougeiro@gmail.com>
2026-09-16 03:02:44 +0000

    fix: add number and numeric type checks in getType() method

--- src/Schema.php
+++ src/Schema.php
@@ -227,6 +227,8 @@
                 'string'   => is_string($value),
                 'int'      => is_int($value),
                 'float'    => is_float($value),
+                'number'   => is_int($value) && is_float($value),
+                'numeric'  => is_numeric($value),
                 'array'    => is_array($value),
                 'object'   => is_object($value),
                 'resource' => is_resource($value),
